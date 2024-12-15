31731d160bd147134a855825873727672203e130
Mat-took <149509482+Mat-took@users.noreply.github.com>
2024-12-15 23:05:42 +0000

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,16 @@
+// Initialize Particles.js for snowfall
+particlesJS("snow", {
+    "particles": {
+      "number": { "value": 150 },
+      "size": { "value": 3 },
+      "opacity": { "value": 0.8 },
+      "move": { "direction": "bottom", "speed": 2 },
+      "color": { "value": "#ffffff" }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": { "enable": false },
+        "onclick": { "enable": false }
+      }
+    }
+  });
